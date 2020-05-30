22431cbb308d96c0dcdb87954115fa032b7da1e6
SimHongsub <ghdtjq2038@gmail.com>
2020-05-31 00:10:16 +0900

    [alloctest] test code for Best Fit algorithm

--- projects/2/alloctest.c
+++ projects/2/alloctest.c
@@ -4,5 +4,49 @@ void alloctest(void)
 {
 	printf("implement page allocation test!!!\n");
 
+	
+	/* SimHongSub : First Fit, Next Fit test code */
+	/*size_t page = palloc_get_multiple(0, 2);
+
 	palloc_get_status(0);
+
+	palloc_free_multiple(page, 1);
+
+	palloc_get_status(0);
+
+	size_t page2 = palloc_get_multiple(0, 1);
+
+	palloc_get_status(0);*/
+
+	/* SimHongSub : Best Fit test code */
+	palloc_get_status(0);
+
+	size_t page = palloc_get_multiple(0, 4);
+
+	palloc_get_status(0);
+
+	palloc_free_multiple(page, 3);
+
+	palloc_get_status(0);
+
+	size_t page2 = palloc_get_multiple(0, 4);
+
+	palloc_get_status(0);
+	
+	palloc_free_multiple(page2, 2);
+
+	palloc_get_status(0);
+
+	size_t page3 = palloc_get_multiple(0, 4);
+
+	palloc_get_status(0);
+
+	palloc_free_multiple(page3, 2);
+
+	palloc_get_status(0);
+
+	size_t page4 = palloc_get_multiple(0, 1);
+
+	palloc_get_status(0);
+
 }
