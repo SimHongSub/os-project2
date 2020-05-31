f1b867d118ef7793db98a2405978fb0191c479d4
SimHongsub <ghdtjq2038@gmail.com>
2020-05-31 17:05:54 +0900

    [alloctest] add buddy system test code

--- projects/2/alloctest.c
+++ projects/2/alloctest.c
@@ -19,7 +19,7 @@ void alloctest(void)
 	palloc_get_status(0);*/
 
 	/* SimHongSub : Best Fit test code */
-	palloc_get_status(0);
+	/*palloc_get_status(0);
 
 	size_t page = palloc_get_multiple(0, 4);
 
@@ -47,6 +47,30 @@ void alloctest(void)
 
 	size_t page4 = palloc_get_multiple(0, 1);
 
+	palloc_get_status(0);*/
+
+	/* SimHongSub : Buddy System test code */
 	palloc_get_status(0);
+
+	size_t page = palloc_get_multiple(0, 4);
+
+	palloc_get_status(0);
+
+	palloc_free_multiple(page, 3);
+
+	palloc_get_status(0);
+
+	size_t page2 = palloc_get_multiple(0, 3);
+
+	palloc_get_status(0);
+	
+	palloc_free_multiple(page2, 2);
+
+	palloc_get_status(0);
+
+	size_t page3 = palloc_get_multiple(0, 1);
+
+	palloc_get_status(0);
+
 
 }
